52ae0fa4a62374d9d8d136daf28f578312cbee4b
milanzderadicka-glitch <milan.zderadicka@gmail.com>
2026-04-06 17:52:25 +0200

    Update app.py

--- app.py
+++ app.py
@@ -1,10 +1,17 @@
+import os
 from flask import Flask
 
 app = Flask(__name__)
 
 @app.route("/")
 def home():
-    return "Aplikace běží"
+    client_id = os.getenv("STRAVA_CLIENT_ID")
+    client_secret = os.getenv("STRAVA_CLIENT_SECRET")
+
+    if client_id and client_secret:
+        return "Aplikace běží a vidi Strava udaje."
+    else:
+        return "Aplikace běží, ale nevidi Strava udaje."
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=10000)
